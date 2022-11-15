cac3c91bccb6abe5e401e69e111fd1ba82b49911
Jesus Ortiz <jesusito_och@hotmail.com>
2022-11-14 21:51:24 -0500

    Cambios hasta antes del video 84 de la seccion 7

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,17 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html'
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar( ){
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
+  }
+
+}
